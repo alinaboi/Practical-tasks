ccd8136fd1693e28efe0d468a71ed51840376edf
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-09 18:40:14 +0200

    Revert "tests with PageObject were added"

--- elements/button.js
+++
@@ -1,7 +0,0 @@
-import BaseElement from "../base/base.element.js";
-export default class Button extends BaseElement {
-    constructor(wdioElement, name) {
-        super(wdioElement, name)
-    }
-    
-};
