32b79c71699f19af9cabfab274df170c7fa913b5
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-18 19:12:05 +0200

    updated profile.page.js

--- pages/profile.page.js
+++ pages/profile.page.js
@@ -4,26 +4,31 @@ import Input from "../elements/input.js";
 import Label from "../elements/label.js";
 
 
-class ProfilePage extends BasePage{
+class ProfilePage extends BasePage {
     constructor() {
         super();
     }
-    
+
     static getBaseElement() {
         return new BaseElement($('.mdl-layout-title'), "Site Logo");
     }
+
     get closePopupBtn() {
         return new Button($('button.close-dialog'), "Close dialog message");
     }
+
     get closeCookieBtn() {
         return new Button($('.cc-btn'), "Closse cookie message");
     }
+
     get backBtn() {
         return new Button($('//span[contains(text(),"Back")]'), "Navigate back to Search Page");
     }
+
     get choosePicInput() {
         return new Input($('//input[@id="picture"]'), "Upload the picture here");
     }
+
     get uploadPicBtn() {
         return new Button($('//button[@aria-label="Button to upload the profile picture"]'), "Save changes");
     }
@@ -47,8 +52,9 @@ class ProfilePage extends BasePage{
             await this.closePopupBtn.click();
         if (await this.closeCookieBtn.isExisting())
             await this.closeCookieBtn.click();
-            await allure.endStep(`passed`);
+        await allure.endStep(`passed`);
     }
+
     async waitForScreenToBeAvailable() {
         await this.backBtn.waitForDisplayed();
         await this.uploadPicBtn.waitForDisplayed();
@@ -67,7 +73,6 @@ class ProfilePage extends BasePage{
         await allure.endStep(`passed`);
     }
 
-   
-    
 }
+
 export default new ProfilePage();
