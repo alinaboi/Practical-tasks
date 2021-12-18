7ffd37be5e59e3d719f1902563bfbed5f6e77c79
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-18 19:06:21 +0200

    updated negativeMobileNumber.js

--- test/specs/negativeMobileNumber.js
+++ test/specs/negativeMobileNumber.js
@@ -4,22 +4,19 @@ import AddressCreatePage from "../../pages/address-create.page.js"
 import AddressSavedPage from "../../pages/address-saved.page.js"
 import addressCreatePage from "../../pages/address-create.page.js";
 import RegistrationViaApi from "../../api/registration.api.js";
-import chai from "chai";
 
 describe('Negative Address creation testing ', async () => {
     it('adding new address with wrong mobile number input', async () => {
         //Precondition ->Registration via API
-        let registrationApi = new RegistrationViaApi("TestuserAB121@gmail.com", "Testuser11");
-        const response = await registrationApi.register();
-        chai.expect(response.status).to.equal(201);
+        const user = await RegistrationViaApi.registerAndReturnUser();
 
         await MainPage.open();
         await MainPage.waitForScreenToBeAvailable();
         await MainPage.openAccountMenu();
-        await MainPage.loginBtn.wdioElement.waitForClickable({ timeout: 5000 });
+        await MainPage.loginBtn.wdioElement.waitForClickable({timeout: 5000});
         await MainPage.navigateToLogin();
         await LoginPage.waitForScreenToBeAvailable();
-        await LoginPage.login("TestuserAB121@gmail.com", "Testuser11");
+        await LoginPage.login(user.password, user.email);
 
         //Search Page ->Address Saved Page
         await MainPage.openAccountMenu();
@@ -33,9 +30,8 @@ describe('Negative Address creation testing ', async () => {
         await AddressCreatePage.waitForScreenToBeAvailable();
         await AddressCreatePage.fillAddressFields("Ukraine", "Test User", "063,3330", "79000", "Rynok Square, 1", "Lviv", "Lvivs'ka");
         await addressCreatePage.submit();
-       
-        //verify if Error massege displayed
+
+        //verify if Error message displayed
         await expect(addressCreatePage.invalidMobileNumberMsg.wdioElement).toBeDisplayed();
-        
     });
 });
