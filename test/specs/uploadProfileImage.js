5ed287a14bc711c8af1096f859b50dbba6b5f11f
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-18 19:08:56 +0200

    updated uploadProfileImage.js

--- test/specs/uploadProfileImage.js
+++ test/specs/uploadProfileImage.js
@@ -7,10 +7,8 @@ import path from "path"
 
 describe('Editing profile info testing ', async () => {
     it('uploading user\'s profile image', async () => {
-        //Precondition ->Registration via API
-        let registrationApi = new RegistrationViaApi("Testuser99999@gmail.com", "Testuser11");
-        const response = await registrationApi.register();
-        chai.expect(response.status).to.equal(201);
+        // Precondition -> Registration via API
+        const user = await RegistrationViaApi.registerAndReturnUser();
 
         await MainPage.open();
         await MainPage.waitForScreenToBeAvailable();
@@ -20,17 +18,17 @@ describe('Editing profile info testing ', async () => {
         });
         await MainPage.navigateToLogin();
         await LoginPage.waitForScreenToBeAvailable();
-        await LoginPage.login('Testuser99999@gmail.com', 'Testuser11');
+        await LoginPage.login(user.email, user.password);
         await MainPage.waitForScreenToBeAvailable();
         await MainPage.openAccountMenu();
         await MainPage.navigateToProfile();
 
-        //Profile Page ->uploading image
+        // Profile Page -> uploading image
 
         const input = ProfilePage.choosePicInput.wdioElement;
         const submitBtn = ProfilePage.uploadPicBtn.wdioElement;
 
-        //store test file path
+        // store test file path
         const filePath = await path.join(path.resolve(), '/image/picture2.jpg');
 
         // use browser.uploadFile to upload the test file
@@ -40,7 +38,7 @@ describe('Editing profile info testing ', async () => {
         await submitBtn.click();
 
         await browser.waitUntil(
-            async () => '/assets/public/images/uploads/default.svg' !== ProfilePage.pictureElement.getAttribute('src'), {
+            async () => '/assets/public/images/uploads/default.svg' !== await ProfilePage.pictureElement.getAttribute('src'), {
                 timeout: 3000
             });
         const srcElement = await ProfilePage.pictureElement.getAttribute('src');
