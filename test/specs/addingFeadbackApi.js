d81872d5c7d8beaf450bf5f1a709c6df12a77068
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-19 20:42:54 +0200

    Update addingFeadbackApi.js

--- test/specs/addingFeadbackApi.js
+++ test/specs/addingFeadbackApi.js
@@ -2,23 +2,24 @@ import RegistrationViaApi from "../../api/registration.api.js";
 import LoginViaApi from "../../api/login.api.js";
 import FeadbackApi from "../../api/feadback.api.js";
 import chai from "chai";
-import userApi from "../../api/user.api.js";
+import UserApi from "../../api/user.api.js";
 
 describe('API testing', async () => {
     it('adding Feadback via API', async () => {
-        
+
         //Registration via API
         const user = await RegistrationViaApi.registerAndReturnUser();
+
         // Login
         const response = await LoginViaApi.loginAndSetToken(user.email, user.password);
         chai.expect(response.status).to.be.equal(200);
 
         //Getting UserID
-        const userResponse = await userApi.getMyself();
-        console.log("OOOOAlina" + JSON.stringify(userResponse));
-        
+        const userId = await UserApi.getMyUserId();
+        console.log('My userId is: ' + userId);
+
         //Sending Feadback
-        //const feadbackResponse = await FeadbackApi.sendFeadback()
+        const feadbackResponse = await FeadbackApi.sendFeadback(userId,);
 
 
     });
