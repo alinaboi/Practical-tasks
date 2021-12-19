c345d35841cb5312915afccd798fc2b83875b6b2
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-19 20:45:35 +0200

    Update positiveLogin.js

--- test/specs/positiveLogin.js
+++ test/specs/positiveLogin.js
@@ -11,7 +11,7 @@ describe('Login testing', async () => {
         await MainPage.open();
         await MainPage.waitForScreenToBeAvailable();
         await MainPage.openAccountMenu();
-        await MainPage.loginBtn.wdioElement.waitForClickable({timeout: 5000});
+        await MainPage.loginBtn.wdioElement.waitForClickable({timeout: 10000});
         await MainPage.navigateToLogin();
 
         //Login Page
