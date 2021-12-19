bf41618ae5c35060b1eb8b72bcbba90d0209bb21
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-20 00:24:43 +0200

    Update registrationApi.js

--- test/specs/registrationApi.js
+++ test/specs/registrationApi.js
@@ -2,15 +2,14 @@ import RegistrationViaApi from "../../api/registration.api.js";
 import chai from "chai";
 
 describe('API testing', async () => {
-    it('Regisration via API ', async () => {
+    it('Registration via API ', async () => {
         
         //Registration via API
         const email = await RegistrationViaApi.generateEmail(5);
         const response = await RegistrationViaApi.register(email, "password");
         chai.expect(response.status).to.be.equal(201);
-
     });
-    it('Negative regisration via API / The user is already exist', async () => {
+    it('Negative registration via API / The user is already exist', async () => {
         
         //Registration via API
         const email = await RegistrationViaApi.generateEmail(5);
@@ -19,6 +18,5 @@ describe('API testing', async () => {
 
         const wrongResponse = await RegistrationViaApi.register(email, "password");
         chai.expect(wrongResponse.status).to.be.equal(400);
-
     });
 });
