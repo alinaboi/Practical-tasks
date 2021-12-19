831531018e4f865100a378d7ac30e1e690fe473f
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-19 20:42:22 +0200

    Update user.api.js

--- api/user.api.js
+++ api/user.api.js
@@ -1,11 +1,16 @@
 import baseApi from "../base/base.api.js";
 
-class userApi {
+class UserApi {
     constructor() {}
 
     async getMyself() {
         return await baseApi.get("rest/user/whoami");
     }
+
+    async getMyUserId() {
+        const userResponse = await this.getMyself();
+        return JSON.parse(userResponse.text).user.id;
+    }
 }
 
-export default new userApi();
+export default new UserApi();
