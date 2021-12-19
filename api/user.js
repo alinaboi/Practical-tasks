6323f846a7c88d21d58d8fd2810d6321b55bd0ed
Alina Boiarchuk <87969442+alinaboi@users.noreply.github.com>
2021-12-19 20:41:25 +0200

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,7 +1,16 @@
-export default class User {
-    constructor(email, password) {
-        this.email = email;
-        this.password = password;
+import baseApi from "../base/base.api.js";
+
+class UserApi {
+    constructor() {}
+
+    async getMyself() {
+        return await baseApi.get("rest/user/whoami");
     }
 
+    async getMyUserId() {
+        const userResponse = await this.getMyself();
+        return JSON.parse(userResponse.text).user.id;
+    }
 }
+
+export default new UserApi();
