2b85ca85d631a7284b792f055d043cbe6bd6a5cc
alinaboi <alinaboyarchuk28@gmail.com>
2023-06-29 00:30:30 +0300

    Products' data updated

--- data/product_data/products.js
+++ data/product_data/products.js
@@ -1,4 +1,12 @@
 export const PRODUCTS = {
-    "appleJuice": { "id": 1 },
-    //TODO : add more items
+    "appleJuice": { "id": 1, "text": "Apple Juice"},
+    "carrotJuice": { "id": 0, "text": "Carrot Juice"},
+    "greenJuice": { "id": 0, "text": "Green Smoothie"},
+    "lemonJuice": { "id": 0, "text": "Lemon Juice"},
+    "orangeJuice": { "id": 0, "text": "Orange Juice"},
+    "eggfruitJuice": { "id": 0, "text": "Eggfruit Juice"},
+    "salesmanArtwork": { "id": 0, "text": "Salesman Artwork"},
+    "permafrost2020Edition": { "id": 0, "text": " Juice Shop"},
+    "melonBike": { "id": 0, "text": "Melon Bike"}
+    //TODO : update ID
 }
