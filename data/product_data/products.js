5b5c26ef04e3b62c4ebec7e0f0695ad00d4c72cf
alinaboi <alinaboyarchuk28@gmail.com>
2023-06-29 00:54:23 +0300

    prettier added

--- data/product_data/products.js
+++ data/product_data/products.js
@@ -1,14 +1,14 @@
 export const PRODUCTS = {
-    "appleJuice": { "id": 1, "text": "Apple Juice", "searchOrder": 1},
-    "applePomace": { "id": 24, "text": "Apple Pomace", "searchOrder": 2},
-    "bananaJuice": { "id": 6, "text": "Banana Juice", "searchOrder": 3},
-    "carrotJuice": { "id": 30, "text": "Carrot Juice", "searchOrder": 5},
-    "greenSmoothie": { "id": 22, "text": "Green Smoothie", "searchOrder": 8},
-    "lemonJuice": { "id": 5, "text": "Lemon Juice", "searchOrder": 10},
-    "orangeJuice": { "id": 2, "text": "Orange Juice", "searchOrder": 30},
-    "eggfruitJuice": { "id": 3, "text": "Eggfruit Juice", "searchOrder": 6},
-    "salesmanArtwork": { "id": 42, "text": "Salesman Artwork", "searchOrder": 4},
-    "permafrost2020Edition": { "id": 41, "text": " Juice Shop", "searchOrder": 9},
-    "melonBike": { "id": 33, "text": "Melon Bike", "searchOrder": 11},
-    "strawberryJuice": { "id": 29, "text": "Strawberry Juice", "searchOrder": 34}
-}
+  appleJuice: { id: 1, text: "Apple Juice", searchOrder: 1 },
+  applePomace: { id: 24, text: "Apple Pomace", searchOrder: 2 },
+  bananaJuice: { id: 6, text: "Banana Juice", searchOrder: 3 },
+  carrotJuice: { id: 30, text: "Carrot Juice", searchOrder: 5 },
+  greenSmoothie: { id: 22, text: "Green Smoothie", searchOrder: 8 },
+  lemonJuice: { id: 5, text: "Lemon Juice", searchOrder: 10 },
+  orangeJuice: { id: 2, text: "Orange Juice", searchOrder: 30 },
+  eggfruitJuice: { id: 3, text: "Eggfruit Juice", searchOrder: 6 },
+  salesmanArtwork: { id: 42, text: "Salesman Artwork", searchOrder: 4 },
+  permafrost2020Edition: { id: 41, text: " Juice Shop", searchOrder: 9 },
+  melonBike: { id: 33, text: "Melon Bike", searchOrder: 11 },
+  strawberryJuice: { id: 29, text: "Strawberry Juice", searchOrder: 34 },
+};
--- pages/search.page.js
+++ pages/search.page.js
@@ -4,195 +4,226 @@ import Dropdown from "../elements/dropdown.js";
 import Label from "../elements/label.js";
 
 class SearchPage extends BasePage {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    static getBaseElement() {
-        return new BaseElement($(''), "");
-    }
+  static getBaseElement() {
+    return new BaseElement($(""), "");
+  }
 
-    get accountMenuBtn() {
-        return new Button($('#navbarAccount'), "Account Menu");
-    }
+  get accountMenuBtn() {
+    return new Button($("#navbarAccount"), "Account Menu");
+  }
 
-    get closePopupBtn() {
-        return new Button($('button.close-dialog'), "Close dialog message");
-    }
+  get closePopupBtn() {
+    return new Button($("button.close-dialog"), "Close dialog message");
+  }
 
-    get closeCookieBtn() {
-        return new Button($('.cc-btn'), "Closse cookie message");
-    }
+  get closeCookieBtn() {
+    return new Button($(".cc-btn"), "Closse cookie message");
+  }
 
-    get sideNavMenuBtn() {
-        return new Button($('.mat-focus-indicator.mat-tooltip-trigger.mat-button.mat-button-base:first-of-type'), "Open Side Navigation Menu");
-    }
+  get sideNavMenuBtn() {
+    return new Button(
+      $(
+        ".mat-focus-indicator.mat-tooltip-trigger.mat-button.mat-button-base:first-of-type"
+      ),
+      "Open Side Navigation Menu"
+    );
+  }
 
-    //(//span[contains(text(),"Add to Basket")])
-    ////button[@class="mat-focus-indicator btn-basket mat-button mat-raised-button mat-button-base mat-primary ng-star-inserted"]
-    get addAppleJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[1]'), "Add Apple Juice to Basket");
-    }
+  //(//span[contains(text(),"Add to Basket")])
+  ////button[@class="mat-focus-indicator btn-basket mat-button mat-raised-button mat-button-base mat-primary ng-star-inserted"]
+  get addAppleJuiceBtn() {
+    return new Button(
+      $('(//button[@aria-label="Add to Basket"])[1]'),
+      "Add Apple Juice to Basket"
+    );
+  }
 
-    get addApplePomaceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[2]'), "Add Apple Pomace to Basket");
-    }
+  get addApplePomaceBtn() {
+    return new Button(
+      $('(//button[@aria-label="Add to Basket"])[2]'),
+      "Add Apple Pomace to Basket"
+    );
+  }
 
-    get addBananaJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[3]'), "");
-    }
+  get addBananaJuiceBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[3]'), "");
+  }
 
-    get addCarrotJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[5]'), "");
-    }
+  get addCarrotJuiceBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[5]'), "");
+  }
 
-    get addEggfruitJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[6]'), "");
-    }
+  get addEggfruitJuiceBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[6]'), "");
+  }
 
-    get addGreenSmoothieBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[8]'), "");
-    }
+  get addGreenSmoothieBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[8]'), "");
+  }
 
-    get addLemonJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[10]'), "");
-    }
+  get addLemonJuiceBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[10]'), "");
+  }
 
-    get addOrangeJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[30]'), "");
-    }
+  get addOrangeJuiceBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[30]'), "");
+  }
 
-    get addSalesmanArtworkBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[4]'), "");
-    }
+  get addSalesmanArtworkBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[4]'), "");
+  }
 
-    get addPermafrost2020EditionBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[9]'), "");
-    }
+  get addPermafrost2020EditionBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[9]'), "");
+  }
 
-    get addMelonBikeBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[11]'), "");
-    }
+  get addMelonBikeBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[11]'), "");
+  }
 
-    get addStrawberryJuiceBtn() {
-        return new Button($('(//button[@aria-label="Add to Basket"])[34]'), "");
-    }
+  get addStrawberryJuiceBtn() {
+    return new Button($('(//button[@aria-label="Add to Basket"])[34]'), "");
+  }
 
-    get ithemsPerPageDropdown() {
-        return new Dropdown($("//div[starts-with(@class,'mat-select-value')]"), "Change the quantity of items on the page")
-    }
+  get ithemsPerPageDropdown() {
+    return new Dropdown(
+      $("//div[starts-with(@class,'mat-select-value')]"),
+      "Change the quantity of items on the page"
+    );
+  }
 
-    get footer() {
-        return new Label($('.mat-paginator.mat-elevation-z6'), "The footer ");
-    }
+  get footer() {
+    return new Label($(".mat-paginator.mat-elevation-z6"), "The footer ");
+  }
 
-    get soldOutLabel() {
-        return new Label($$('.ribbon.ribbon-top-left.ribbon-sold.ng-star-inserted'), "Sold Out Mark ");
-    }
+  get soldOutLabel() {
+    return new Label(
+      $$(".ribbon.ribbon-top-left.ribbon-sold.ng-star-inserted"),
+      "Sold Out Mark "
+    );
+  }
 
-    get onlyThreeLeftLabel() {
-        return new Label($('(//span[contains(text(), "Only 3 left")])'), "Only 3 Left Mark ");
-    }
+  get onlyThreeLeftLabel() {
+    return new Label(
+      $('(//span[contains(text(), "Only 3 left")])'),
+      "Only 3 Left Mark "
+    );
+  }
 
-    get onlyTwoLeftLabel() {
-        return new Label($('(//span[contains(text(), "Only 2 left")])'), "Only 2 Left Mark ");
-    }
+  get onlyTwoLeftLabel() {
+    return new Label(
+      $('(//span[contains(text(), "Only 2 left")])'),
+      "Only 2 Left Mark "
+    );
+  }
 
-    get basketBtn() {
-        return new Button($('[aria-label="Show the shopping cart"]'), "Navigate to Basket");
-    }
+  get basketBtn() {
+    return new Button(
+      $('[aria-label="Show the shopping cart"]'),
+      "Navigate to Basket"
+    );
+  }
 
-    //action methods
-    async open() {
-        await allure.startStep(`Navigation to the Search Page`);
-        await super.open(`${global.baseUrl}#/search`);
-        /*if (await this.closePopupBtn.isExisting())
+  //action methods
+  async open() {
+    await allure.startStep(`Navigation to the Search Page`);
+    await super.open(`${global.baseUrl}#/search`);
+    /*if (await this.closePopupBtn.isExisting())
             await this.closePopupBtn.click();
         if (await this.closeCookieBtn.isExisting())
             await this.closeCookieBtn.click();*/
-        await allure.endStep(`passed`);
-    }
+    await allure.endStep(`passed`);
+  }
 
-    async waitForScreenToBeAvailable() {
-        await this.accountMenuBtn.waitForDisplayed();
-        await this.sideNavMenuBtn.waitForDisplayed();
-        await this.footer.waitForDisplayed();
-        await this.addMelonBikeBtn.waitForDisplayed();
-    }
+  async waitForScreenToBeAvailable() {
+    await this.accountMenuBtn.waitForDisplayed();
+    await this.sideNavMenuBtn.waitForDisplayed();
+    await this.footer.waitForDisplayed();
+    await this.addMelonBikeBtn.waitForDisplayed();
+  }
 
-    async addAppleJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addAppleJuiceBtn.click();
-    }
+  async addAppleJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addAppleJuiceBtn.click();
+  }
 
-    async addApplePomace() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addApplePomaceBtn.click();
-    }
+  async addApplePomace() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addApplePomaceBtn.click();
+  }
 
-    async addBananaJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addBananaJuiceBtn.click();
-    }
+  async addBananaJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addBananaJuiceBtn.click();
+  }
 
-    async addCarrotJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addCarrotJuiceBtn.click();
-    }
+  async addCarrotJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addCarrotJuiceBtn.click();
+  }
 
-    async addEggfruitJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addEggfruitJuiceBtn.click();
-    }
+  async addEggfruitJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addEggfruitJuiceBtn.click();
+  }
 
-    async addGreenSmoothie() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addGreenSmoothieBtn.click();
-    }
+  async addGreenSmoothie() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addGreenSmoothieBtn.click();
+  }
 
-    async addLemonJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addLemonJuiceBtn.click();
-    }
+  async addLemonJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addLemonJuiceBtn.click();
+  }
 
-    async addOrangeJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addOrangeJuiceBtn.click();
-    }
+  async addOrangeJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addOrangeJuiceBtn.click();
+  }
 
-    async addStrawberryJuice() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addStrawberryJuiceBtn.click();
-    }
+  async addStrawberryJuice() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addStrawberryJuiceBtn.click();
+  }
 
-    async addSalesmanArtwork() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addSalesmanArtworkBtn.wdioElement.waitForClickable({ timeout: 10000 });
-        await this.addSalesmanArtworkBtn.click();
-    }
+  async addSalesmanArtwork() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addSalesmanArtworkBtn.wdioElement.waitForClickable({
+      timeout: 10000,
+    });
+    await this.addSalesmanArtworkBtn.click();
+  }
 
-    async addPermafrost2020Edition() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addPermafrost2020EditionBtn.wdioElement.waitForClickable({ timeout: 10000 });
-        await this.addPermafrost2020EditionBtn.click();
-    }
+  async addPermafrost2020Edition() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addPermafrost2020EditionBtn.wdioElement.waitForClickable({
+      timeout: 10000,
+    });
+    await this.addPermafrost2020EditionBtn.click();
+  }
 
-    async addMelonBike() {
-        await allure.addStep(`Click on Add to Basket Button`);
-        await this.addMelonBikeBtn.wdioElement.waitForClickable({ timeout: 10000 });
-        await this.addMelonBikeBtn.click();
-    }
+  async addMelonBike() {
+    await allure.addStep(`Click on Add to Basket Button`);
+    await this.addMelonBikeBtn.wdioElement.waitForClickable({ timeout: 10000 });
+    await this.addMelonBikeBtn.click();
+  }
 
-    async changeIthemsQuantity(text) {
-        await allure.addStep(`Change the quantity of items on the page`);
-        await this.ithemsPerPageDropdown.wdioElement.waitForClickable({
-            timeout: 10000
-        });
-        await this.ithemsPerPageDropdown.select(text);
-    }
+  async changeIthemsQuantity(text) {
+    await allure.addStep(`Change the quantity of items on the page`);
+    await this.ithemsPerPageDropdown.wdioElement.waitForClickable({
+      timeout: 10000,
+    });
+    await this.ithemsPerPageDropdown.select(text);
+  }
 
-    async openBasket() {
-        await allure.addStep(`Click on the Basket Button`);
-        await this.basketBtn.click();
-    }
-} export default new SearchPage();
+  async openBasket() {
+    await allure.addStep(`Click on the Basket Button`);
+    await this.basketBtn.click();
+  }
+}
+export default new SearchPage();
