5f0e73d2b445ae9a66c577c36454358bf2c751c0
alinaboi <alinaboyarchuk28@gmail.com>
2023-06-29 00:46:59 +0300

    products' data updated

--- data/product_data/products.js
+++ data/product_data/products.js
@@ -1,12 +1,14 @@
 export const PRODUCTS = {
-    "appleJuice": { "id": 1, "text": "Apple Juice"},
-    "carrotJuice": { "id": 0, "text": "Carrot Juice"},
-    "greenJuice": { "id": 0, "text": "Green Smoothie"},
-    "lemonJuice": { "id": 0, "text": "Lemon Juice"},
-    "orangeJuice": { "id": 0, "text": "Orange Juice"},
-    "eggfruitJuice": { "id": 0, "text": "Eggfruit Juice"},
-    "salesmanArtwork": { "id": 0, "text": "Salesman Artwork"},
-    "permafrost2020Edition": { "id": 0, "text": " Juice Shop"},
-    "melonBike": { "id": 0, "text": "Melon Bike"}
-    //TODO : update ID
+    "appleJuice": { "id": 1, "text": "Apple Juice", "searchOrder": 1},
+    "applePomace": { "id": 24, "text": "Apple Pomace", "searchOrder": 2},
+    "bananaJuice": { "id": 6, "text": "Banana Juice", "searchOrder": 3},
+    "carrotJuice": { "id": 30, "text": "Carrot Juice", "searchOrder": 5},
+    "greenSmoothie": { "id": 22, "text": "Green Smoothie", "searchOrder": 8},
+    "lemonJuice": { "id": 5, "text": "Lemon Juice", "searchOrder": 10},
+    "orangeJuice": { "id": 2, "text": "Orange Juice", "searchOrder": 30},
+    "eggfruitJuice": { "id": 3, "text": "Eggfruit Juice", "searchOrder": 6},
+    "salesmanArtwork": { "id": 42, "text": "Salesman Artwork", "searchOrder": 4},
+    "permafrost2020Edition": { "id": 41, "text": " Juice Shop", "searchOrder": 9},
+    "melonBike": { "id": 33, "text": "Melon Bike", "searchOrder": 11},
+    "strawberryJuice": { "id": 29, "text": "Strawberry Juice", "searchOrder": 34}
 }
