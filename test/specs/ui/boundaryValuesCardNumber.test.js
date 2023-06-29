7b3ad62718cb6c2eef1bded66ea572160f701c0d
alinaboi <alinaboyarchuk28@gmail.com>
2023-06-30 02:29:40 +0300

    boundaryValuesCardNumber test automated

--- test/specs/ui/boundaryValuesCardNumber.test.js
+++ test/specs/ui/boundaryValuesCardNumber.test.js
@@ -2,16 +2,15 @@ import LoginPage from "../../../pages/login.page.js";
 import MainPage from "../../../pages/main.page.js";
 import SearchPage from "../../../pages/search.page.js";
 import PaymentShopPage from "../../../pages/payment-shop.page.js";
-import OrderCompletionPage from "../../../pages/order-completion.page.js";
 import RegistrationViaApi from "../../../api/registration.api.js";
 
 import { data } from "../../../data/test_data/00002_data.js";
 
-describe("Full purchase flow testing ", async () => {
+describe("Boundary Values. Card Number field", async () => {
   let user;
 
-  before("Register user using API method", async () => {
-    //Precondition -> Registration via API
+  before("Register user and log in", async () => {
+    //Precondition -> Registration via API & UI Login
     user = await RegistrationViaApi.registerAndReturnUser();
     await MainPage.open();
     await MainPage.waitForScreenToBeAvailable();
@@ -19,29 +18,57 @@ describe("Full purchase flow testing ", async () => {
     await MainPage.navigateToLogin();
     await LoginPage.waitForScreenToBeAvailable();
     await LoginPage.login(user.email, user.password);
+  });
 
-});
-
-it("15", async () => {
-    //
+  it("Try fifteen digits card number", async () => {
+    //Navigating to Payment Page
     await SearchPage.open();
     await SearchPage.waitForScreenToBeAvailable();
     await MainPage.openAccountMenu();
     await MainPage.navigateToOrdersAndPayment();
     await MainPage.navigateToPayment();
 
-    //
     await PaymentShopPage.waitForScreenToBeAvailable();
     await PaymentShopPage.clickAddNewCard();
     await PaymentShopPage.waitForScreenToBeAvailable();
 
-    //
+    //Try fifteen digits card number
     await PaymentShopPage.fillName(data.fullName);
     await PaymentShopPage.fillCardNumber(data.fifteenDigitsCard);
     await PaymentShopPage.selectExpiryMonth(data.expMonth);
     await PaymentShopPage.selectExpiryYear(data.expYear);
 
-    //
-    await expect(PaymentShopPage.wrongCardNumberLengthMsg.wdioElement).toBeDisplayed();
+    //Verify Error is displayed
+    await expect(
+      PaymentShopPage.wrongCardNumberLengthMsg.wdioElement
+    ).toBeDisplayed();
+  });
+
+  it("Try seventeen digits card number", async () => {
+    await allure.addIssue(
+      `!Bug found : System allows to add card with 17 digits lenght!`
+    );
+
+    //Try seventeen digits card number
+    await PaymentShopPage.fillCardNumber(data.seventeenDigitsCard);
+    await PaymentShopPage.personNameInput.click();
+
+    //Verify Error is displayed
+    await expect(
+      PaymentShopPage.wrongCardNumberLengthMsg.wdioElement
+    ).toBeDisplayed();
+  });
+
+  it("Try sixteen digits card number", async () => {
+    //Try sixteen digits card number
+    await PaymentShopPage.fillCardNumber(data.sixteenDigitsCard);
+    await PaymentShopPage.personNameInput.click();
+
+    //Verify Error is NOT displayed and Submit button is clickable
+    await expect(
+      PaymentShopPage.wrongCardNumberLengthMsg.wdioElement
+    ).not.toBeDisplayed();
+    await expect(PaymentShopPage.submitBtn.wdioElement).toBeEnabled();
+    await expect(PaymentShopPage.submitBtn.wdioElement).toBeClickable();
   });
 });
