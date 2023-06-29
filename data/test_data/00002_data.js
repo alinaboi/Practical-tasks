559be7e3d0eb3a48331fdd1efc0db0bba815d5ce
alinaboi <alinaboyarchuk28@gmail.com>
2023-06-30 01:34:43 +0300

    data file created

---
+++ data/test_data/00002_data.js
@@ -0,0 +1,8 @@
+export const data = {
+  fullName: "Test Person",
+  expMonth: "3",
+  expYear: "2092",
+  fifteenDigitsCard: "123451234512345",
+  sixteenDigitsCard: "1234567890987654",
+  seventeenDigitsCard: "09876543212345678",
+};
